34bb2ba67979f0e69f8758b642d7ce597bdaaf59
Santhosh <Santhosh@Santhosh>
2020-10-16 09:27:51 +0530

    Added product class through br1

---
+++ Onlineshopping/src/com/lti/model/product.java
@@ -0,0 +1,27 @@
+package com.lti.model;
+
+public class product {
+  int productId;
+  String produceName;
+  String productDesc;
+public int getProductId() {
+	return productId;
+}
+public void setProductId(int productId) {
+	this.productId = productId;
+}
+public String getProduceName() {
+	return produceName;
+}
+public void setProduceName(String produceName) {
+	this.produceName = produceName;
+}
+public String getProductDesc() {
+	return productDesc;
+}
+public void setProductDesc(String productDesc) {
+	this.productDesc = productDesc;
+}
+  
+  
+}
