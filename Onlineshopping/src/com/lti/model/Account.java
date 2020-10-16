63b510afcba6f2a1122ae531f276f10e929f8e90
Santhosh <Santhosh@Santhosh>
2020-10-16 09:42:35 +0530

    Added account class through br1

---
+++ Onlineshopping/src/com/lti/model/Account.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Account {
+int accId;
+double balance;
+public int getAccId() {
+	return accId;
+}
+public void setAccId(int accId) {
+	this.accId = accId;
+}
+public double getBalance() {
+	return balance;
+}
+public void setBalance(double balance) {
+	this.balance = balance;
+}
+
+}
